f86c2826cca8854ce196e1c80b3700dbc470cd71
guitxc <guisandolidna@gmail.com>
2025-02-11 00:40:50 -0300

    Try, except parte 1 e 2

--- aula106.py
+++ aula106.py
@@ -1,1 +1,21 @@
-# Try
+# Try, except, else e finally
+
+try:
+    a = 18
+    b = 0
+    print('linha 1.'[1000])
+    c = a / b
+    print('linha 2.') # pula direto para o except
+
+except ZeroDivisionError:
+    print('Dividiu por zero!.')
+except NameError:
+    print('Não esta definido')
+except (TypeError, IndexError) as error: # Jogar a instancia em uma variavel
+    print('TypeError + IndexError')
+    print('MSG:', error)
+    print('Nome:', error.__class__.__name__)
+except Exception: # Trata todos os outros erros
+    print('Erro desconhcido')
+
+print('Continuar')
