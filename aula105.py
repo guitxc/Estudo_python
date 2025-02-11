1184080ef1ce46e137ee20689a28378826688f23
guitxc <guisandolidna@gmail.com>
2025-02-11 00:20:34 -0300

    Yield from

--- aula105.py
+++ aula105.py
@@ -1,3 +1,15 @@
-# Introdução às Generator functions em Python
-# generator = (n for n in range(1000000))
+# Yield from
+def gen1():
+    yield 1
+    yield 2
+    yield 3
 
+def gen2():
+    yield from gen1() # posso continuar um yield da onde a outra função parou!
+    yield 4
+    yield 5
+    yield 6
+
+g = gen2()
+for numero in g:
+    print(numero)
