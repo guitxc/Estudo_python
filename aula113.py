39c5db5e047dc2c47e6a49d8ff7f2043d3516acb
guitxc <guisandolidna@gmail.com>
2025-03-20 17:21:47 -0300

    mais jeitos de importar e organizar modulos

--- aula113.py
+++ aula113.py
@@ -1,9 +1,17 @@
-from sys import path
+# from sys import path
 
-import aula113_package.modulo
+# import aula113_package.modulo
 
-from aula113_package import soma_do_modulo
+# from aula113_package import soma_do_modulo
 
-# print(path,  sep='\n')
+# # print(path,  sep='\n')
 
-print(soma_do_modulo(1, 2))
+# print(soma_do_modulo(1, 2))
+from aula113_package.modulo_b import fala_oi
+
+
+
+print(__name__)
+# fala_oi()
+
+# todas as importações tem que ser relativas ao main
--- aula113_package/modulo.py
+++ aula113_package/modulo.py
@@ -1,8 +1,13 @@
-__all__ = [
-    'variavel',
-    'soma_do_modulo',
-]
+# __all__ = [
+#     'variavel',
+#     'soma_do_modulo',
+# ]
+from modulo_b import fala_oi
 
 variavel = 'Alguma coisa'
+
+
 def soma_do_modulo(x, y):
     return x + y
+
+fala_oi()
